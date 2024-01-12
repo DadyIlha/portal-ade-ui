39f51928f45ee5a8e776aacd377f4f95b8e9a1d2
Wallace Vidal <wallacevff@hotmail.com>
2024-01-12 13:27:24 -0300

    Alteração Endpoints

--- src/app/api/powerBiApi/token/route.ts
+++ src/app/api/powerBiApi/token/route.ts
@@ -10,17 +10,16 @@ export async function POST(req: NextRequest) {
         "groupId": process.env.SEMED_DADOSGERAIS_GROUP_ID,
         "reportId": process.env.SEMED_DADOSGERAIS_REPORT_ID
     }
+    var bodyReq = await req.json();
     var clientId: string | undefined = body.clientId;
     var groupId: string | undefined = body.groupId;
     var reportId: string | undefined = body.reportId;
-    var token: string = (await axios.post(`${process.env.LOCALAPI_PROTOCOL}://${process.env.LOCALAPI_URL}:${process.env.LOCALAPI_PORT}/api/powerBiApi/accessToken`, {
-        client_id: clientId
-    })).data;
+    
     var url: string = `https://api.powerbi.com/v1.0/myorg/groups/${groupId}/reports/${reportId}/GenerateToken`;
     var headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
-        "Authorization": token
+        "Authorization": bodyReq.token
     };
     var reportToken = (await axios.post(url, {
         "accessLevel": "View",
@@ -29,5 +28,5 @@ export async function POST(req: NextRequest) {
         headers: headers
     })).data;
     // var b = await req.json();
-    return NextResponse.json(reportToken.token);
+    return NextResponse.json(reportToken);
 }
