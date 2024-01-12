c853b837e098d8e285ba80cd6f7731beeeef8337
Wallace Vidal <wallacevff@hotmail.com>
2024-01-12 12:31:10 -0300

    Correção build

--- src/app/api/powerBiApi/token/route.ts
+++ src/app/api/powerBiApi/token/route.ts
@@ -1,18 +1,18 @@
-
+"use server";
 import axios, { AxiosHeaders } from "axios";
 import { Route } from "lucide-react";
 // import type { NextApiRequest, NextApiResponse } from 'next';
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
     var body = {
-        "clientId": process.env.APPLICATION_ID!,
-        "groupId": process.env.SEMED_DADOSGERAIS_GROUP_ID!,
-        "reportId": process.env.SEMED_DADOSGERAIS_REPORT_ID!
+        "clientId": process.env.APPLICATION_ID,
+        "groupId": process.env.SEMED_DADOSGERAIS_GROUP_ID,
+        "reportId": process.env.SEMED_DADOSGERAIS_REPORT_ID
     }
-    var clientId: string = body.clientId;
-    var groupId: string = body.groupId;
-    var reportId: string = body.reportId;
+    var clientId: string | undefined = body.clientId;
+    var groupId: string | undefined = body.groupId;
+    var reportId: string | undefined = body.reportId;
     var token: string = (await axios.post(`${process.env.LOCALAPI_PROTOCOL}://${process.env.LOCALAPI_URL}:${process.env.LOCALAPI_PORT}/api/powerBiApi/accessToken`, {
         client_id: clientId
     })).data;
