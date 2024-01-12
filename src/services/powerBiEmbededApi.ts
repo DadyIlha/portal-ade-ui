f0266b8a07b6883ceebaafd72d9272ac5f58e990
Wallace Vidal <wallacevff@hotmail.com>
2024-01-12 11:02:33 -0300

    .Env

--- src/services/powerBiEmbededApi.ts
+++ src/services/powerBiEmbededApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function getReportToken() {
-    return (await axios.post(`/api/powerBiApi/token`, {
+    return (await axios.post(`${process.env.NEXT_PUBLIC_LOCALAPI_PROTOCOL}//${process.env.NEXT_PUBLIC_LOCALAPI_URL}:${process.env.NEXT_PUBLIC_LOCALAPI_PORT}/api/powerBiApi/token`, {
         "clientId": process.env.NEXT_PUBLIC_APPLICATION_ID,
         "groupId": process.env.NEXT_PUBLIC_SEMED_DADOSGERAIS_GROUP_ID,
         "reportId": process.env.NEXT_PUBLIC_SEMED_DADOSGERAIS_REPORT_ID
