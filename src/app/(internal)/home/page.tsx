68a7fb4be76ef11a438515a88c2ae2d5ca310ddf
Wallace Vidal <wallacevff@hotmail.com>
2024-01-13 20:20:38 -0300

    Power BI Integrado

--- src/app/(internal)/home/page.tsx
+++ src/app/(internal)/home/page.tsx
@@ -1,3 +1,33 @@
+"use client"
+import { models } from 'powerbi-client';
+import { PowerBIEmbed } from 'powerbi-client-react';
+
+import {getReportToken} from '@/services/powerBiEmbededApi';
+import { useEffect, useState } from 'react';
+
+function Report(props: any){
+  return <PowerBIEmbed 
+  embedConfig={{
+    type: 'report',   // Supported types: report, dashboard, tile, visual, qna and paginated report
+    id: process.env.NEXT_PUBLIC_SEMED_DADOSGERAIS_REPORT_ID,
+    embedUrl: `https://app.powerbi.com/reportEmbed?reportId=${process.env.NEXT_PUBLIC_SEMED_DADOSGERAIS_REPORT_ID}&groupId=${process.env.NEXT_PUBLIC_SEMED_DADOSGERAIS_GROUP_ID}`,
+    accessToken: props.token,    // Keep as empty string, null or undefined
+    tokenType: models.TokenType.Embed
+  }}
+/>
+}
+
+
 export default function Home() {
-  return <div></div>
+  const [token, setToken] = useState('');
+  useEffect(() =>
+  {
+    const GetToken = async () => {setToken(await getReportToken()); console.log(token); };
+    GetToken();
+  }, []);
+  return (<div className='w-screen h-screen'>
+
+    {<Report token={token}/>}
+  </div>
+  );
 }
